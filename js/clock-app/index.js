93fdcc63350ffb22c6ba1f13f0fb1e477c23503b
richardsforu <praveen.somireddy@gmail.com>
2020-11-30 17:33:51 +0530

    clock-app

---
+++ js/clock-app/index.js
@@ -0,0 +1,26 @@
+
+let indianClock = document.querySelector('#indianClock');
+let dubaiClock = document.querySelector('#dubaiClock');
+let usClock = document.querySelector('#usClock');
+
+
+
+let jsClock = (props) => {
+
+    let date = new Date().toLocaleTimeString('en-US',{timeZone:props});
+
+    let ui = `
+    <div>${props}</div>
+    <p> ${date} </p>
+    `
+
+    return ui;
+}
+
+
+setInterval(()=>{
+    indianClock.innerHTML=jsClock('Asia/kolkata');
+    dubaiClock.innerHTML=jsClock('Asia/dubai');
+    usClock.innerHTML=jsClock('America/New_York');
+
+},1000);
